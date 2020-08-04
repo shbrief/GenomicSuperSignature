e7aa638a8f59380fee4f9fcd63c1662dcd0ada99
Sehyun Oh <shbrief@hotmail.com>
2020-08-04 15:20:33 -0400

    Add msigdb_gsea function

--- R/funcForGSEA_plot.R
+++ R/funcForGSEA_plot.R
@@ -5,15 +5,23 @@
 #' @param category A character vector representing MSigDB category. Options are
 #' "H","C1","C2"(default), "C3","C4","C5","C6", and "C7"
 #' @param n An interger. The number of top and bottom enriched pathways to plot. Default is 10.
+#' @param pvalueCutoff Cutoff for both pvalue and p.adjust. Default is 0.5.
 #'
-#' @return Barplot of GSEA output. Top and bottom \code{n} genesets are plotted.
+#' @return Barplot of GSEA output. Top and bottom \code{n} genesets based on NES
+#' are plotted and qvalues are denoted by color.
 #'
 #' @export
-msigdb_gsea <- function(ind, PCAmodel, category = "C2", n = 10) {
+msigdb_gsea <- function(ind, PCAmodel, category = "C2",
+                        n = 10, pvalueCutoff = 0.5) {
 
     ## Target geneList
     al <- model(PCAmodel)[, ind]
-    names(al) <- AnnotationDbi::mapIds(org.Hs.eg.db, keys=names(al), column='ENTREZID', keytype='SYMBOL')
+    # names(al) <- AnnotationDbi::mapIds(org.Hs.eg.db::org.Hs.eg.db, keys=names(al),
+    #                                    column='ENTREZID', keytype='SYMBOL')
+
+    obj <- list()
+    obj[[1]] <- names(al)
+    names(al) <- EnrichmentBrowser::idMap(obj, "hsa", from = "SYMBOL", to = "ENTREZID")[[1]]
     al <- sort(al, decreasing = TRUE)
 
     ## Formating
@@ -23,7 +31,7 @@ msigdb_gsea <- function(ind, PCAmodel, category = "C2", n = 10) {
     ## MSigDB
     m <- msigdbr::msigdbr(species = "Homo sapiens", category = category) %>%
         clusterProfiler.dplyr::select(gs_name, entrez_gene)
-    ms <- clusterProfiler::GSEA(geneList, TERM2GENE = m)
+    ms <- clusterProfiler::GSEA(geneList, TERM2GENE = m, pvalueCutoff = pvalueCutoff)
 
     ## Barplot
     y <- clusterProfiler.dplyr::mutate(ms, ordering = abs(NES)) %>% clusterProfiler.dplyr::arrange(desc(ordering))
@@ -32,28 +40,4 @@ msigdb_gsea <- function(ind, PCAmodel, category = "C2", n = 10) {
         geom_bar(stat='identity') +
         scale_fill_continuous(low='red', high='blue', guide=guide_colorbar(reverse=TRUE)) +
         theme_minimal() + ylab(NULL)
-}
-
-
-
-
-#' Dotplot of Gene Ontology Enrichment result
-#'
-#' @param x Available options are "GeneRatio" (default) and "Count"
-#' @param color "Available options are "pvalue", "p.adjust" (default), and "qvalue"
-#' @export
-do_dotplot <- function(ind, PCAmodel, x = "GeneRatio", color = "p.adjust") {
-
-    ## Target geneList
-    al <- model(PCAmodel)[, ind]
-    names(al) <- AnnotationDbi::mapIds(org.Hs.eg.db, keys=names(al), column='ENTREZID', keytype='SYMBOL')
-    al <- sort(al, decreasing = TRUE)
-
-    ## Formating
-    geneList <- al
-    gene <- names(geneList)[abs(geneList) > mean(abs(geneList))]
-
-    edo2 <- DOSE::gseDO(geneList)
-    dotplot(edo2, showCategory = 10, font.size = 8,
-            x = x, color = color)
 }
