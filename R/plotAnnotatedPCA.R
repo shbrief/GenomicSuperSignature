c0ce9a2c65c6140aa954ba915acc816121e60e76
Sehyun Oh <shbrief@hotmail.com>
2021-07-20 15:13:39 -0400

    collate error fix

--- R/plotAnnotatedPCA.R
+++ R/plotAnnotatedPCA.R
@@ -40,52 +40,52 @@ plotAnnotatedPCA <- function(dataset, RAVmodel, PCnum, val_all = NULL,
                              color_by = NULL, color_lab = NULL,
                              trimed_pathway_len = 45) {
 
-  # Extract expression matrix from different classes
-  dat <- .extractExprsMatrix(dataset)
+    # Extract expression matrix from different classes
+    dat <- .extractExprsMatrix(dataset)
 
-  if (is.null(val_all)) {val_all <- validate(dat, RAVmodel)}
-  PCAres <- stats::prcomp(dat)$rotation %>% as.data.frame
+    if (is.null(val_all)) {val_all <- validate(dat, RAVmodel)}
+    PCAres <- stats::prcomp(dat)$rotation %>% as.data.frame
 
-  # two PCs to plot
-  ind1 <- which(colnames(PCAres) == paste0("PC", PCnum[1]))
-  ind2 <- which(colnames(PCAres) == paste0("PC", PCnum[2]))
+    # two PCs to plot
+    ind1 <- which(colnames(PCAres) == paste0("PC", PCnum[1]))
+    ind2 <- which(colnames(PCAres) == paste0("PC", PCnum[2]))
 
-  if (is.null(color_lab)) {color_lab = "Color By"}
-  if (!is.null(color_by)) {
-    colorFeature <- utils::stack(color_by) %>%
-      tibble::column_to_rownames(var = "ind")
-    PCAres <- cbind(PCAres, colorFeature)
-    myPlot <- ggplot(PCAres, mapping = aes_string(x = names(PCAres)[ind1],
-                                                  y = names(PCAres)[ind2],
-                                                  color = "values")) +
-      labs(color = color_lab) +
-      geom_point()
-  } else {
-    myPlot <- ggplot(PCAres, mapping = aes_string(x = names(PCAres)[ind1],
-                                                  y = names(PCAres)[ind2])) +
-      geom_point()
-  }
+    if (is.null(color_lab)) {color_lab = "Color By"}
+    if (!is.null(color_by)) {
+        colorFeature <- utils::stack(color_by) %>%
+            tibble::column_to_rownames(var = "ind")
+        PCAres <- cbind(PCAres, colorFeature)
+        myPlot <- ggplot(PCAres, mapping = aes_string(x = names(PCAres)[ind1],
+                                                      y = names(PCAres)[ind2],
+                                                      color = "values")) +
+            labs(color = color_lab) +
+            geom_point()
+    } else {
+        myPlot <- ggplot(PCAres, mapping = aes_string(x = names(PCAres)[ind1],
+                                                      y = names(PCAres)[ind2])) +
+            geom_point()
+    }
 
-  # Trim the long pathway names
-  annotatedPC <- annotatePC(c(ind1, ind2), val_all, RAVmodel,
-                            scoreCutoff = scoreCutoff, nesCutoff = nesCutoff,
-                            trimed_pathway_len = trimed_pathway_len)
+    # Trim the long pathway names
+    annotatedPC <- annotatePC(c(ind1, ind2), val_all, RAVmodel,
+                              scoreCutoff = scoreCutoff, nesCutoff = nesCutoff,
+                              trimed_pathway_len = trimed_pathway_len)
 
-  # PC annotation table - flextable version
-  flextable::set_flextable_defaults(na_str = "NA")
-  myTable <- flextable::flextable(annotatedPC) %>% flextable::width(width = 2.5)
-  myTable <- grid::rasterGrob(flextable::as_raster(myTable))
+    # PC annotation table - flextable version
+    flextable::set_flextable_defaults(na_str = "NA")
+    myTable <- flextable::flextable(annotatedPC) %>% flextable::width(width = 2.5)
+    myTable <- grid::rasterGrob(flextable::as_raster(myTable))
 
-  # # PC annotation table - ggpubr version
-  # myTable <- ggpubr::ggtexttable(annotatedPC,
-  #                                rows = NULL,
-  #                                theme = ggpubr::ttheme("mOrange"))
-  # myTable <- ggpubr::table_cell_font(myTable,
-  #                                    row = 2:ggpubr::tab_nrow(myTable),
-  #                                    column = 1:2, size = 9)
+    # # PC annotation table - ggpubr version
+    # myTable <- ggpubr::ggtexttable(annotatedPC,
+    #                                rows = NULL,
+    #                                theme = ggpubr::ttheme("mOrange"))
+    # myTable <- ggpubr::table_cell_font(myTable,
+    #                                    row = 2:ggpubr::tab_nrow(myTable),
+    #                                    column = 1:2, size = 9)
 
-  res <- ggpubr::ggarrange(myPlot, myTable,
-                           ncol = 1, nrow = 2, heights = c(1, 0.5))
-  print(res)
+    res <- ggpubr::ggarrange(myPlot, myTable,
+                             ncol = 1, nrow = 2, heights = c(1, 0.5))
+    print(res)
 }
 
