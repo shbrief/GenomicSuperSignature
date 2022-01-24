212c6f32baf60460d69f0efa67a29b880ef44cbd
Sehyun Oh <shbrief@hotmail.com>
2022-01-24 15:04:58 -0500

    subsetEnrichedPathways function returns NES

--- R/subsetEnrichedPathways.R
+++ R/subsetEnrichedPathways.R
@@ -12,6 +12,8 @@
 #' @param both Default is \code{FALSE}, where only the top \code{n} pathways
 #' will be printed. If it is set to \code{TRUE}, the output will contain both
 #' top and bottom \code{n} pathways.
+#' @param include_nes Defalt is \code{FALSE}. If it set to \code{TRUE}, the
+#' output will include both description and NES of the enriched pathway.
 #'
 #' @return A DataFrame with top and bottom \code{n} pathways from the
 #' enrichment results.
@@ -27,45 +29,49 @@
 #'
 #'
 #' @export
-subsetEnrichedPathways <- function(RAVmodel, ind = NULL, n = 10, both = FALSE) {
+subsetEnrichedPathways <- function(RAVmodel, ind = NULL, n = 10,
+                                   both = FALSE, include_nes = FALSE) {
 
+  ## Take the GSEA annotation
   if (is(RAVmodel, "PCAGenomicSignatures")) {
     gsea_loading <- gsea(RAVmodel)
   } else if(is.data.frame(RAVmodel) | is.matrix(RAVmodel) | is.list(RAVmodel)) {
     gsea_loading <- RAVmodel
   }
 
+  ## RAVs to subset GSEA annotation from
   res <- list()
-  for (name in names(gsea_loading)) {
+  if (is.null(ind)) {
+    names <- names(gsea_loading)
+  } else {names <- paste0("RAV", ind)}
+
+  ## Subset the enriched pathways with top/bottom NES
+  for (name in names) {
     x <- gsea_loading[[name]]
     if (is.null(dim(x))) {
       res[[name]] <- "There is no enriched pathway"
     } else {
-      up <- x$Description[order(x$NES, decreasing=TRUE)][seq_len(n)]
-      down <- x$Description[order(x$NES, decreasing=FALSE)][seq_len(n)]
-      res[[name]] <- c(up, down)
+      y <- x[,c("Description", "NES")]
+      up <- y[order(y$NES, decreasing=TRUE),][seq_len(n),]
+      rownames(up) <- paste0("Up_", seq_len(n))
+      down <- y[order(y$NES, decreasing=FALSE),][seq_len(n),]
+      rownames(down) <- paste0("Down_", seq_len(n))
+      res[[name]] <- rbind(up, down)
     }
-
-    ### Adding NES and qvalues?
-    # y <- x[,c("Description", "NES", "qvalues")]
-    # up <- y[order(y$NES, decreasing=TRUE),][seq_len(n),]
-    # down <- y[order(y$NES, decreasing=FALSE),][seq_len(n),]
-    # res[[name]] <- rbind(up, down)
   }
 
+  ## Remove NES if it is requested
+  if (!include_nes) {
+    for (name in names) {
+      res[[name]] <- res[[name]][,"Description", drop = FALSE]
+      colnames(res[[name]]) <- paste(name, colnames(res[[name]]), sep = ".")
+    }
+  }
+
+  ## Combine list into one data frame
   res <- data.frame(res, check.names = FALSE)
-  rnames <- c(paste0("Up_", seq_len(n)), paste0("Down_", seq_len(n)))
-  rownames(res) <- rnames
+  if (!both) {res <- res[seq_len(n),,drop=FALSE]}
 
-  if (!both) {res <- res[seq_len(n),]}
-
-  if (is.null(ind)) {
-    res <- S4Vectors::DataFrame(res)
-    return(res)
-  } else {
-    col_num <- which(colnames(res) %in% c(paste0("RAV", ind)))
-    res <- res[, col_num, drop = FALSE]
-    res <- S4Vectors::DataFrame(res)
-    return(res)
-  }
+  res <- S4Vectors::DataFrame(res)
+  return(res)
 }
