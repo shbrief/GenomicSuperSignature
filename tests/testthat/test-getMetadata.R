eff6be666c59be8951f2d3ce5875ce4ba528a9a8
Sehyun Oh <shbrief@hotmail.com>
2022-02-13 11:35:01 -0500

    unit tests

---
+++ tests/testthat/test-getMetadata.R
@@ -0,0 +1,22 @@
+context("Test getRAVInfo and getStudyinfo functions")
+
+data(miniRAVmodel)
+
+test_that("getRAVInfo", {
+    res <- getRAVInfo(miniRAVmodel, ind = 438)
+    val <- c(5,7,9,3,5,6)
+
+    expect_true(is.list(res))
+    expect_equal(length(res), 4)
+    expect_true(all(unlist(res[1:3]) == c(6, 0.04, 44)))
+    expect_equal(res[[4]]$PC, val)
+})
+
+test_that("getStudyInfo", {
+    res2 <- getStudyInfo(miniRAVmodel, "SRP119465")
+    val2 <- c("studyTitle", "studySize", "RAVs")
+
+    expect_equal(names(res2), val2)
+    expect_equal(dim(res2$RAVs), c(3, 3))
+    expect_equal(res2$studySize, 100)
+})
