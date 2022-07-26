0f8d1d25d8415499b052e8ad2eb4baf6d935c5a3
Sehyun Oh <shbrief@hotmail.com>
2022-07-26 19:35:39 -0400

    update GitHub pages

---
+++ inst/scripts/rowNorm.R
@@ -0,0 +1,15 @@
+#' z-score each row of the data
+#'
+#' @param x A gene-expression matrix with genes in rows and samples in columns
+#' @references  \url{https://github.com/shbrief/PLIER/blob/master/R/Allfuncs.R}
+#'
+#' @return a matrix with each row mean centered and scaled by rowwise sd
+#'
+#' @examples
+#' x = matrix(rnorm(100),nc=10)
+#' y = rowNorm(x)
+#' apply(y,1,mean)
+#'
+rowNorm <- function(x){
+  return(t(scale(t(x))))
+}
