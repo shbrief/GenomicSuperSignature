bbda3fc8d970446d32e220dd44053f8ff54a372c
Sehyun Oh <shbrief@hotmail.com>
2025-10-15 11:56:29 -0400

    update to follow the new column names

--- inst/scripts/searchPathways.R
+++ inst/scripts/searchPathways.R
@@ -3,14 +3,19 @@
 #' From the GSEA output of each RAV, subset the enriched pathways with the
 #' minimum q-value. Also, this function keeps only \code{Description, NES, qvalues}.
 #'
-#' @param RAVmodel PCAGenomicSignatures object.
+#' @param RAVmodel PCAGenomicSignatures object
 #' @param gsea.dir "~/data2/PCAGenomicSignatureLibrary/refinebioRseq/RAVmodel_536/gsea"
+#' @param min.qval Under the default (\code{TRUE}), only the enriched pathways
+#' with the minimum qvalue will be saved. If it is set \code{FALSE}, all the
+#' enriched pathways pass the \code{pvalueCutoff} of the function
+#' \code{\link[clusterProfiler]{GSEA}} will be extracted.
+#'
 #' @return A list with the length of RAVs in the provided RAVmodel. Each
 #' element contains the subset data.frame of GSEA output.
 #'
 #' @note This function is for model construction, not for end-users.
 #'
-searchPathways <- function(RAVmodel, gsea.dir) {
+searchPathways <- function(RAVmodel, gsea.dir, min.qval = TRUE) {
 
   ## If you want to select only a subset of RAVs with the specific cluster size
   # ind <- which(metadata(RAVmodel)$size > 3)
@@ -18,22 +23,31 @@ searchPathways <- function(RAVmodel, gsea.dir) {
   # names(gsea_all) <- colnames(RAVmodel)[ind]
 
   gsea_all <- vector(mode = "list", length = ncol(RAVmodel))
-  names(gsea_all) <- colnames(RAVmodel)
+  names(gsea_all) <- paste0("RAV", seq_len(ncol(RAVmodel)))
   gsea.dir <- gsea.dir
 
   for (i in seq_len(ncol(RAVmodel))) {
     pathToRes <- file.path(gsea.dir, paste0("gsea_", i, ".rds"))
-    res <- readRDS(pathToRes)
+    res_ls <- readRDS(pathToRes)
+    res <- res_ls@result
 
-    # If there is no enriched pathways
+    ## If there is no enriched pathways
     if (nrow(res) == 0) {
       resName <- paste0("RAV", i)
-      gsea_all[[resName]] <- res[, c("Description", "NES", "pvalue", "qvalues"), drop = FALSE]
+      gsea_all[[resName]] <- res[, c("Description", "NES", "pvalue", "qvalue"), drop = FALSE]
       print(paste("RAV", i, "has no enriched pathways."))
       next
     }
 
-    res <- res[which(res$qvalues == min(res$qvalues)), c("Description", "NES", "pvalue", "qvalues"), drop = FALSE]
+    ## Select all enriched pathways or only the ones with the minimum qvalue
+    if (isTRUE(min.qval)) {
+        
+        res <- res[which(res$qvalue == min(res$qvalue)),
+                   c("Description", "NES", "pvalue", "qvalue"), drop = FALSE]
+    } else {
+        res <- res[, c("Description", "NES", "pvalue", "qvalue"), drop = FALSE]
+    }
+
     resName <- paste0("RAV", i)
     gsea_all[[resName]] <- res
     print(paste("RAV", i, "is added."))
